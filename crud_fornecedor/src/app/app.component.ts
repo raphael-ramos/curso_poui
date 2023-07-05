608230a12c170d1a7cf6b4460e3133d571e0132b
Raphael Ramos <40366613+raphael-ramos@users.noreply.github.com>
2023-07-05 13:15:46 -0300

    Criadá página para projeto de cadastro de fornecedor com Protheus

---
+++ crud_fornecedor/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { PoMenuItem } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', link:'/', shortLabel:'Home', icon:'po-icon-home' },
+    { label: 'Sair', link:'/', shortLabel:'Sair', icon:'po-icon-exit' },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
+}
